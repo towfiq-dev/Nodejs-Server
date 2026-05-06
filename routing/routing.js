ba1b4b09938facab8c231fb269e8d6d5e55e7a8d
Towfiqul Islam <towfiqulislam017399@gmail.com>
2026-05-06 07:11:48 +0600

    Auto-update on Wed, May  6, 2026  7:11:48 AM

---
+++ routing/routing.js
@@ -0,0 +1,27 @@
+const http = require('http')
+const fs = require('fs')
+const port = 4000
+const hostname = '127.0.0.1'
+
+const server = http.createServer((req, res)=>{
+
+  if (req.url === '/') {
+    fs.readFile("index.html", (err, data)=>{
+    res.writeHead(200, {"Content-Type": "text/html"})
+    res.write(data)
+    res.end()
+    })
+    
+  }else if (req.url === '/about') {
+      fs.readFile("about.html", (err, data)=>{
+        res.writeHead(200, {'Content-Type': 'text/html'})
+        res.write(data)
+        res.end()
+      })
+    }
+})
+
+server.listen(port, hostname, ()=>{
+  console.log(`server is running at http;//${hostname}:${port}`);
+  
+})
